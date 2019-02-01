9a712a1528f13ef6d21882e075bbd32928549531
Sviatoslav <s.granovskiy@gmail.com>
2019-02-01 14:26:58 +0200

    Immutable Class

---
+++ ImmutableClassTask/src/com/gmail/granovskiy/Start.java
@@ -0,0 +1,8 @@
+package com.gmail.granovskiy;
+
+public class Start {
+    public static void main(String[] args) {
+        ImmutablePoint point = new ImmutablePoint(3.2, 5.1);
+        System.out.println(point);
+    }
+}
